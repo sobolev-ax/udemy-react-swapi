c9625b091357fd73bc3c6b6c222a7b8f540560e4
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-27 13:56:09 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Go to stars');
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+
+  return body;
+}
+
+getResource('https://swapi.dev/api/people/1')
+  .then((body) => console.log(body));
