bb716de9bbe70f9fa7d577b532429d0915fc34f6
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-27 15:28:19 +0300

    SwapiService

--- src/index.js
+++ src/index.js
@@ -1,16 +1,80 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
+class SwapiService {
+  async getPeople() {
+    const people = await this._getResource('people');
 
-  if(!res.ok) {
-    throw new Error(`could not fetch:\n${url}` +
-      `\nrecieved ${res.status}`);
+    return people.results;
   }
 
-  const body = await res.json();
+  async getPerson(id) {
+    const person = await this._getResource(`people/${id}`);
 
-  return body;
+    return person;
+  }
+
+  async getPlanets() {
+    const people = await this._getResource('planets');
+
+    return people.results;
+  }
+
+  async getPlanet(id) {
+    const person = await this._getResource(`planets/${id}`);
+
+    return person;
+  }
+
+  async getStarships() {
+    const people = await this._getResource('starships');
+
+    return people.results;
+  }
+
+  async getStarship(id) {
+    const person = await this._getResource(`starships/${id}`);
+
+    return person;
+  }
+
+
+  _apiBase = 'https://swapi.dev/api';
+
+  async _getResource(url) {
+    const fullUrl = `${this._apiBase}/${url}`;
+    const res = await fetch(fullUrl);
+
+    if(!res.ok) {
+      throw new Error(`could not fetch:\n${fullUrl}` +
+        `\nrecieved ${res.status}`);
+    }
+  
+    const body = await res.json();
+  
+    return body;
+  }
 }
 
-getResource('https://swapi.dev/api/people/1')
+const swapi = new SwapiService();
+
+swapi.getPeople()
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
+
+swapi.getPerson(1)
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
+
+swapi.getPlanets()
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
+
+swapi.getPlanet(1)
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
+
+swapi.getStarships()
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
+
+swapi.getStarship(9)
   .then((body) => console.log(body))
   .catch((err) => console.error('getResource,', err));
