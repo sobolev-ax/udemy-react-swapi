51eb97028ee1a70fec67a0690bec8ee0defd9f8c
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-27 19:55:19 +0300

    Random

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,56 @@ import Header from '../header';
 import List from '../list';
 import Details from '../details';
 
+import SwapiService from '../../services/swapi';
+
 import './app.css';
 
 export default class App extends Component {
+
+  _swapiService = new SwapiService();
+  _maxId = 0;
+
+  updatePlanet = async () => {
+    const id = Math.floor(Math.random() * 5 + 2);
+    const planet = await this._swapiService.getPlanet(id);
+
+    this.setState({
+      random: {
+        header: planet.name,
+        img: `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`,
+        data: [
+          this.createDetail('Population', planet.population),
+          this.createDetail('Rotation Period', planet.rotation_period),
+          this.createDetail('Diameter', planet.diameter),
+        ],
+      }
+    })
+  }
+
+  state = {
+    random: {
+      header: 'Random Block',
+      img: '',
+      data: [],
+    }
+  }
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  createDetail(label, value) {
+    return {
+      label,
+      value,
+      key: this._maxId++
+    }
+  }
+
   render() {
+    const { random: { header, img, data }} = this.state;
+
     return (
       <main>
 
@@ -17,7 +63,7 @@ export default class App extends Component {
 
           <div className="row mb-5">
             <div className="col">
-              <Details />
+              <Details header={ header } img={ img } data={ data } />
             </div>
           </div>
 
--- src/components/details/details.js
+++ src/components/details/details.js
@@ -2,35 +2,31 @@ import React from 'react';
 
 import './details.css';
 
-const Details = () => {
+const Details = ({ header, img, data = [] }) => {
   return (
     <div className="card bg-light border-secondary shadow">
       <div className="row">
         <div className="col-md-4">
-          <svg className="card-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+          <img src={ img } alt={ header } height="100%" width="100%" className="card-img" />
         </div>
         <div className="col-md-8">
           <div className="card-body">
 
             <h3 className="card-title">
-              Planet Name
+              { header }
             </h3>
 
             <ul className="list-group list-group-flush">
-              <li className="list-group-item bg-light">
-                <span className="font-weight-bold mr-1">Population:</span>
-                123124
-              </li>
-
-              <li className="list-group-item bg-light">
-                <span className="font-weight-bold mr-1">Rotation Period:</span>
-                43
-              </li>
-
-              <li className="list-group-item bg-light">
-                <span className="font-weight-bold mr-1">Diameter:</span>
-                100
-              </li>
+              {
+                data.map(({ label, value, key }) => {
+                  return (
+                    <li key={ key } className="list-group-item bg-light">
+                      <span className="font-weight-bold mr-2">{label}:</span>
+                      { value }
+                    </li>
+                  )
+                })
+              }
             </ul>
 
           </div>
