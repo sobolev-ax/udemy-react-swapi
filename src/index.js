523e8d3abcbc97fe4fcd70c2e10585339079d549
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-27 14:08:36 +0300

    Error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok) {
+    throw new Error(`could not fetch:\n${url}` +
+      `\nrecieved ${res.status}`);
+  }
+
   const body = await res.json();
 
   return body;
 }
 
 getResource('https://swapi.dev/api/people/1')
-  .then((body) => console.log(body));
+  .then((body) => console.log(body))
+  .catch((err) => console.error('getResource,', err));
